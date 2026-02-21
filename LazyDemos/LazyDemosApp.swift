3b3ff02ba7334d4a61f3e39022da4c63d0df813c
Jim Walejko <jwalejko@me.com>
2026-02-20 20:22:34 -0700

    Initial Commit

---
+++ LazyDemos/LazyDemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyDemosApp.swift
+//  LazyDemos
+//
+//  Created by Jim Walejko on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct LazyDemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
